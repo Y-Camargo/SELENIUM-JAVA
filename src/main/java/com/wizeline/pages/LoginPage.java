9f0de055daeb3bb56ffa95cce71a95edcb43af39
Yeimmy Paola Camargo <yeimmyuseche24@gmail.com>
2025-08-10 22:25:26 -0500

    Add job and configuration for lint LoginPage

--- src/main/java/com/wizeline/pages/LoginPage.java
+++ src/main/java/com/wizeline/pages/LoginPage.java
@@ -7,36 +7,36 @@ import org.openqa.selenium.WebElement;
 
 public class LoginPage extends BasePage {
 
-    private By usernameField = By.id("user-name");
-    private By passwordField = By.id("password");
-    private By loginButton = By.id("login-button");
-    private By errorMessage = By.cssSelector("h3[data-test='error']");
+  private By usernameField = By.id("user-name");
+  private By passwordField = By.id("password");
+  private By loginButton = By.id("login-button");
+  private By errorMessage = By.cssSelector("h3[data-test='error']");
 
-    public LoginPage(WebDriver driver) {
-        super(driver);
-    }
+  public LoginPage(WebDriver driver) {
+    super(driver);
+  }
 
-    // Navega a la página de login
-    public void openLoginPage() {
-        System.out.println("Abriendo la URL: " + Config.BASE_URL);
-        navigateTo(Config.BASE_URL);
-    }
+  // Navega a la página de login
+  public void openLoginPage() {
+    System.out.println("Abriendo la URL: " + Config.BASE_URL);
+    navigateTo(Config.BASE_URL);
+  }
 
-    // Inicia sesión con un nombre de usuario y una contraseña
-    public void login(String username, String password) {
-        writeText(usernameField, username);
-        writeText(passwordField, password);
-        click(loginButton);
-    }
+  // Inicia sesión con un nombre de usuario y una contraseña
+  public void login(String username, String password) {
+    writeText(usernameField, username);
+    writeText(passwordField, password);
+    click(loginButton);
+  }
 
-    // Verifica que la página de login esté visible
-    public boolean isLoginPageDisplayed() {
-        return driver.getTitle().contains("Swag Labs");
-    }
+  // Verifica que la página de login esté visible
+  public boolean isLoginPageDisplayed() {
+    return driver.getTitle().contains("Swag Labs");
+  }
 
-    // Obtiene el mensaje de error
-    public String getErrorMessage() {
+  // Obtiene el mensaje de error
+  public String getErrorMessage() {
     WebElement errorElement = driver.findElement(By.cssSelector("[data-test='error']"));
     return errorElement.getText().trim();
-    }
+  }
 }
